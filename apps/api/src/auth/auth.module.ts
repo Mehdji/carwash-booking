7e867d72fc5dc618142b390cff9f2746d512ed18
Mehdi <naouimehdi@hotmail.fr>
2026-06-21 00:55:00 +0200

    feat(auth): remove jwtConstants and use environment variable for JWT secret

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -4,13 +4,18 @@ import { AuthService } from './auth.service';
 import { UtilisateursModule } from '../utilisateurs/utilisateurs.module';
 import { PasswordService } from './password.service';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './auth.constant';
 
+
+const jwtSecret = process.env.JWT_SECRET;
+
+if (!jwtSecret) {
+  throw new Error('JWT_SECRET is required');
+}
 @Module({
   imports: [UtilisateursModule, JwtModule.register({
     global: true,
-    secret: process.env.NEXTAUTH_SECRET,
-    signOptions: { expiresIn: '60s' },
+    secret: jwtSecret,
+    signOptions: { expiresIn: '15m' },
   })],
   controllers: [AuthController],
   providers: [AuthService, PasswordService]
